13c33722c90c9f7a3df494ebb5dddb979a4c7cee
AugustoChies <guto_chies@hotmail.com>
2018-11-07 01:29:29 -0200

    development commit to final rep

---
+++ Assets/MyScripts/EnemyBeatMove.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyDirection
+{
+    right,left
+};
+
+
+public class EnemyBeatMove : MonoBehaviour {
+    private delegate void Movement();
+    Movement Moveonbeat;
+    public bool moving;
+    public EnemyDirection mydirection;
+    public BPMtimer selectedBeatDetection;
+    public GameObject leftsensor, rightsensor, leftoutersensor, rightoutersensor;
+    //Reset bools from previous action after some loops, to properly let enemies see them in time 
+    private float resetpreviousaction;
+
+    // Use this for initialization
+    void Start () {
+        selectedBeatDetection.OnBPMBeat += OnBeat;
+        resetpreviousaction = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (mydirection == EnemyDirection.left)
+        {            
+            if (leftsensor.GetComponent<TriggerSensor>().isInside)
+            {
+                if (!leftsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().attacking
+                 && !leftsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().leftinput)
+                {
+                    Moveonbeat = leftAttack;
+                }
+                else if(leftsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().leftinput)
+                {
+                    Moveonbeat = moveLeft;
+                    moving = true;
+                }
+                else
+                {
+                    Moveonbeat = null;
+                }
+            }
+            else
+            {
+                if (leftoutersensor.GetComponent<TriggerSensor>().isInside && 
+                    !leftoutersensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<EnemyBeatMove>().moving)
+                {
+                    Moveonbeat = null;
+                }
+                else
+                {
+                    Moveonbeat = moveLeft;
+                    moving = true;
+                }
+            }
+        }
+        else if(mydirection == EnemyDirection.right)
+        {
+            if (rightsensor.GetComponent<TriggerSensor>().isInside)
+            {
+                if (!rightsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().attacking
+                 && !rightsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().rightinput)
+                {
+                    Moveonbeat = rightAttack;
+                }
+                else if (rightsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().rightinput)
+                {
+                    Moveonbeat = moveRight;
+                    moving = true;
+                }
+                else
+                {
+                    Moveonbeat = null;
+                }
+            }
+            else
+            {
+                if (rightoutersensor.GetComponent<TriggerSensor>().isInside &&
+                    !rightoutersensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<EnemyBeatMove>().moving)
+                {
+                    Moveonbeat = null;
+                }
+                else
+                {
+                    Moveonbeat = moveRight;
+                    moving = true;
+                }
+            }
+        }
+
+        if (resetpreviousaction > 0)
+        {
+            resetpreviousaction--;
+            if (resetpreviousaction <= 0)
+            {
+                moving = false;
+            }
+        }
+    }
+
+    void moveRight()
+    {
+        this.transform.Translate(1, 0, 0);
+    }
+
+    void moveLeft()
+    {
+        this.transform.Translate(-1, 0, 0);
+    }
+
+    void rightAttack()
+    {
+        rightsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().life--;
+    }
+
+    void leftAttack()
+    {
+        leftsensor.GetComponent<TriggerSensor>().colidedObject.GetComponent<PlayerBeatMovement>().life--;
+    }
+
+    void OnBeat()
+    {
+        if (Moveonbeat != null)
+            Moveonbeat();
+        Moveonbeat = null;
+        resetpreviousaction = 3;
+    }
+}
